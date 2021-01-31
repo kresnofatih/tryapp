db19fec2f0c5c27428809d11273bcdc9faf3399b
Fatih <kresnofatihimani.private@gmail.com>
2021-01-31 18:05:00 +0700

    ready to push

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [temp, setTemp] = useState('')
+  const [name, setName] = useState('')
+  const changeName = () => {
+    setName(temp)
+    setTemp('')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <StatusBar style="" />
+      <View style={styles.header}>
+        <Text style={styles.title}>{name}</Text>
+      </View>
+      <Text>Enter Name: </Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder="e.g Sakurajima Mai" 
+        onChangeText={(val)=>setTemp(val)} 
+        value={temp}/>
+      <Button title="change" 
+      onPress={changeName}></Button>
     </View>
   );
 }
@@ -14,8 +31,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: "skyblue",
+    padding: 20,
+    width: 200,
+    alignItems: "center"
+  },
+  title: {
+    color: "white",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 5,
+    margin: 5,
+    width: 250,
+  }
 });
